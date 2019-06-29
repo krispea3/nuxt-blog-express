918ab2a2483ec3c5ef3afab0d02a476515d94774
krispea3 <christian.bracher@gmail.com>
2019-06-29 09:18:35 +0800

    Logout when no Token or Token expired

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,10 +1,15 @@
-import { networkInterfaces } from "os";
-
 export default function(context) {
   const token = context.app.$cookies.get('token', {path: '/'})
-  if (context.store.getters.user.idToken) {
-    if (!token) {
-      context.store.commit('logout')
-    }
+  const expirationDate = context.app.$cookies.get('expirationDate')
+  const now = new Date().toISOString()
+
+  function parseISO(s) {
+    s = s.split(/\D/);
+    return new Date(Date.UTC(s[0],--s[1],s[2],s[3],s[4],s[5],s[6]));
+  }
+
+  // Logout if no token or token expired
+  if (!token || parseISO(now) > parseISO(expirationDate)) {
+    context.store.commit('logout')
   }
 }
