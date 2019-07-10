24d4086532711c2c0f99b096e3a6376a16c392eb
krispea3 <christian.bracher@gmail.com>
2019-07-10 13:28:31 +0800

    Changed  axios  baseUrl for Heroku

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,7 @@ module.exports = {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: process.env.NODE_ENV === 'development' ?'http://localhost:3000' :'https://nuxt-blog-express.herokuapp.com',
+    baseURL: process.env.NODE_ENV === 'development' ?'http://localhost:3000' :'http://172.17.116.214:57574/',
     credentials: false
   },
   /*
